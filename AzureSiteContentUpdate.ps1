d619c676610be30dad4e198b207415de6ad7d55d
Mazterhan <olexiy.brydnya@gmail.com>
2019-10-15 18:10:29 +0300

    Finished process for uploading all of the files to FTP with replased if it updated. No changes for Azure WebApp

--- AzureSiteContentUpdate.ps1
+++ AzureSiteContentUpdate.ps1
@@ -2,31 +2,83 @@
 {
 Login-AzureRmAccount  
 Select-AzureRmSubscription -SubscriptionId '6c28b945-6d98-403d-8936-5e658f228a0f'  
-$password = '3v}B~PMWR)5bB&gt'
+$FTPPass = '3v}B~PMWR)5bB&gt' # FTP Server Variables
 if ($SiteName = 'RVD')
    {
    $SiteName          = 'RvdNow'
    $Site_TestURL      = 'https://rvdnow-preprodstage.azurewebsites.net'
    $Site_URL          = 'https://www.rvdnow.com'
-   $sourceFolder      = '\\PRBCWEBTESTX\c$\Projects\MB.rVd\*'
-   $TempZIPFolder     = 'F:\RVD_ftp_site\'
-   $targetFTPFolder   = 'ftp://waws-prod-dm1-015.ftp.azurewebsites.windows.net/site/wwwroot/'
-   $user_TestURL      = 'rvdnow__PreProdStage\optoutadmin1'
+   $UploadFolder      = '\\PRBCWEBTESTX\c$\Projects\MB.rVd\'  #Directory where to find pictures to upload
+   $FTPHost           = 'ftp://waws-prod-dm1-015.ftp.azurewebsites.windows.net/site/wwwroot/' # FTP Server Variables
+   $FTPUser           = 'rvdnow__PreProdStage\optoutadmin1'  # FTP Server Variables
    $sourceslot        = 'rvdnow-PreProdStage'
    $destinationslot   = 'rvdnow'
    $RG                = 'RvdNow' 
    }
 
-#Создание архива данных, перенос
-Compress-Archive -Path $sourceFolder -CompressionLevel Optimal -Update -DestinationPath "$TempZIPFolder $SiteName .zip"
 
-#копирование на ftp server
+clear
+ 
+# FTP Server Variables
+ 
+#Directory where to find pictures to upload
+  
 $webclient = New-Object System.Net.WebClient 
-$webclient.Credentials = New-Object System.Net.NetworkCredential($user_TestURL,$password)  
-$webclient.UploadFile($targetFTPFolder, $TempZIPFolder+$SiteName.zip)
+$webclient.Credentials = New-Object System.Net.NetworkCredential($FTPUser,$FTPPass)  
+ 
+$SrcEntries = Get-ChildItem $UploadFolder -Recurse
+$Srcfolders = $SrcEntries | Where-Object{$_.PSIsContainer}
+$SrcFiles = $SrcEntries | Where-Object{!$_.PSIsContainer}
+ 
+# Create FTP Directory/SubDirectory If Needed - Start
+foreach($folder in $Srcfolders)
+{    
+    $SrcFolderPath = $UploadFolder  -replace "\\","\\" -replace "\:","\:"   
+    $DesFolder = $folder.Fullname -replace $SrcFolderPath,$FTPHost
+    $DesFolder = $DesFolder -replace "\\", "/"
+    # Write-Output $DesFolder
+ 
+    try
+        {
+            $makeDirectory = [System.Net.WebRequest]::Create($DesFolder);
+            $makeDirectory.Credentials = New-Object System.Net.NetworkCredential($FTPUser,$FTPPass);
+            $makeDirectory.Method = [System.Net.WebRequestMethods+FTP]::MakeDirectory;
+            $makeDirectory.GetResponse();
+            #folder created successfully
+        }
+    catch [Net.WebException]
+        {
+            try {
+                #if there was an error returned, check if folder already existed on server
+                $checkDirectory = [System.Net.WebRequest]::Create($DesFolder);
+                $checkDirectory.Credentials = New-Object System.Net.NetworkCredential($FTPUser,$FTPPass);
+                $checkDirectory.Method = [System.Net.WebRequestMethods+FTP]::PrintWorkingDirectory;
+                $response = $checkDirectory.GetResponse();
+                #folder already exists!
+            }
+            catch [Net.WebException] {
+                #if the folder didn't exist
+            }
+        }
+}
+# Create FTP Directory/SubDirectory If Needed - Stop
+ 
+# Upload Files - Start
+foreach($entry in $SrcFiles)
+{
+    $SrcFullname = $entry.fullname
+    $SrcName = $entry.Name
+    $SrcFilePath = $UploadFolder -replace "\\","\\" -replace "\:","\:"
+    $DesFile = $SrcFullname -replace $SrcFilePath,$FTPHost
+    $DesFile = $DesFile -replace "\\", "/"
+    # Write-Output $DesFile
+ 
+    $uri = New-Object System.Uri($DesFile) 
+    $webclient.UploadFile($uri, $SrcFullname)
+}
+# Upload Files - Stop
 
 
-Expand-Archive -LiteralPath "$TempZIPFolder $SiteName.zip" -DestinationPath $targetFTPFolder -Force
 
 #проверка работоспособности тестового сайта
 Start-Process chrome.exe -ArgumentList @( '-incognito', $Site_TestURL )
